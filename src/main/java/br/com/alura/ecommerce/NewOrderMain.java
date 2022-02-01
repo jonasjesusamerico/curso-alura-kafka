f7acdc05c943818468ef4fc5fa353e626d3be1e9
jonasjesusamerico <jjsamerico@gmail.com>
2022-01-31 21:37:27 -0300

    Criando o producer do kafka

---
+++ src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -0,0 +1,43 @@
+package br.com.alura.ecommerce;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class NewOrderMain {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties());
+
+        String value = "12121,23232,34343434";
+        ProducerRecord<String, String> record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
+
+        // Producer permite passar uma função de callback para conseguir criar validações de notificação
+        producer.send(record, (data, ex) -> {
+            if (ex != null) {
+                ex.printStackTrace();
+                return;
+            }
+            System.out.println("Sucesso, enviando no topico:" + data.topic() + "/ partition: " + data.partition() + " / offset: " + data.offset() + " / timeStamp " + data.timestamp());
+        }).get();
+
+    }
+
+    private static Properties properties() {
+        Properties properties = new Properties();
+
+        // Indica qual o caminho que o kafka está respondendo
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+
+        // Diz qual é o tipo de serializer que será reponsavel por transformar a string para bytes tanto do key como do value
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        return properties;
+    }
+
+}
